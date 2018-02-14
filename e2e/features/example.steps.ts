5f84faf783385fae026d896824db63f8307f3456
dartavion <richardranke@gmail.com>
2018-02-13 19:29:45 -0500

    fixes for cukes test

--- e2e/features/example.steps.ts
+++ e2e/features/example.steps.ts
@@ -11,9 +11,13 @@ defineSupportCode(({ Given, Then }) => {
   Given(/^I submit the phone number "([^"]*)"$/, givenCount);
   async function givenCount(phone): Promise<void> {
     await browser.get('http://localhost:4200/phone');
-    browser.getCurrentUrl().then((data) => {
+    await browser.getCurrentUrl().then((data) => {
         expect(data).to.equal('http://localhost:4200/phone');
-        return element(by.model('item.value')).sendKeys(phone);
+        return element(by.css('.phone-input')).sendKeys(phone);
     });
+  }
+  Then(/^I see the formatted phone number "([^"]*)"$/, thenPhone);
+  async function thenPhone(phone): Promise<void> {
+    expect(phone).to.equal('585-865-1122');
   }
 });
