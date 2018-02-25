350c641d54048b94524b695bcb5d1a01f02d6afe
dartavion <richardranke@gmail.com>
2018-02-24 20:25:39 -0500

    adding prettier

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -1,25 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { SwapiService } from '../../services/swapi.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { SwapiService } from "../../services/swapi.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-character',
-  templateUrl: './character.component.html',
-  styleUrls: ['./character.component.css']
+  selector: "app-character",
+  templateUrl: "./character.component.html",
+  styleUrls: ["./character.component.css"]
 })
 export class CharacterComponent implements OnInit {
   private nextUrl;
   private previousUrl;
   private characters;
 
-  constructor(private swapi: SwapiService, private router: Router) { }
+  constructor(private swapi: SwapiService, private router: Router) {}
 
   ngOnInit() {
     this.getCharacters();
   }
+
   goTo(character: string) {
-    this.router.navigate(['/home', { outlets: { characterDetails: ['character-details', character] } }], { skipLocationChange: true });
+    this.router.navigate(
+      [
+        "/home",
+        {
+          outlets: {
+            characterDetails: ["character-details", character]
+          }
+        }
+      ],
+      { skipLocationChange: true }
+    );
   }
+
   next() {
     this.getCharacters(this.nextUrl);
   }
@@ -29,13 +41,17 @@ export class CharacterComponent implements OnInit {
   }
 
   private getCharacters(url?: string) {
-    this.swapi.getCharacters(url)
-      .subscribe((data) => {
-        this.nextUrl = data.next;
-        this.previousUrl = data.previous;
-        this.characters = data.results;
-        this.router.navigate(['/home', { outlets: { characterDetails: ['character-details', 'Luke'] } }], { skipLocationChange: true });
+    this.swapi.getCharacters(url).subscribe(data => {
+      this.nextUrl = data.next;
+      this.previousUrl = data.previous;
+      this.characters = data.results;
+      this.router.navigate(
+        [
+          "/home",
+          { outlets: { characterDetails: ["character-details", "Luke"] } }
+        ],
+        { skipLocationChange: true }
+      );
     });
   }
-
 }
