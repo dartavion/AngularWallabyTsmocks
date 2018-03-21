679e1cf1ba3b5a60194c35f36a344b54ca0b20d8
dartavion <richardranke@gmail.com>
2018-03-21 18:44:48 -0400

    finishing up

--- src/app/components/phone/phone.component.spec.ts
+++ src/app/components/phone/phone.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { PhoneComponent } from './phone.component';
 import { PhonePipe } from '../../pipes/phone.pipe';
